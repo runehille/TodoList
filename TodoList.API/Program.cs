b83b8a90000bc71dcd97c2f590790d9b28a28f0d
Rune <rune.hille@gmail.com>
2023-04-07 14:56:40 +0200

    Enable Swagger in production.

--- TodoList.API/Program.cs
+++ TodoList.API/Program.cs
@@ -14,12 +14,8 @@ builder.Services.AddDbContext<SqlDbContext>(options => options.UseSqlServer("nam
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
