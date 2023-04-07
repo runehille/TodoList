1b0b787473903176598095c0cc5ca5db819c94a7
Rune <rune.hille@gmail.com>
2023-04-07 21:01:51 +0200

    Enable CORS to send test data between Client and API

--- TodoList.API/Program.cs
+++ TodoList.API/Program.cs
@@ -12,12 +12,24 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<SqlDbContext>(options => options.UseSqlServer("name=ConnectionStrings"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+        });
+});
+
 var app = builder.Build();
+
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
