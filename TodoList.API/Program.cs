0aa0683d28652a3232615af2da1ad62ad42b634e
runehille <rune.hille@gmail.com>
2023-09-02 21:08:42 +0200

    Enable Swagger in prod

--- TodoList.API/Program.cs
+++ TodoList.API/Program.cs
@@ -39,9 +39,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
